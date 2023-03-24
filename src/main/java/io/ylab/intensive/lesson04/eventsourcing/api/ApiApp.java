f51a3d4f964dd2a18aff9a0855a3093e1f22f9e0
maksimgurin <gurin.md.pro@gmail.com>
2023-03-24 14:00:47 +0100

    template

--- src/main/java/io/ylab/intensive/lesson04/eventsourcing/api/ApiApp.java
+++ src/main/java/io/ylab/intensive/lesson04/eventsourcing/api/ApiApp.java
@@ -1,7 +1,15 @@
 package io.ylab.intensive.lesson04.eventsourcing.api;
 
+import com.rabbitmq.client.ConnectionFactory;
+import io.ylab.intensive.lesson04.RabbitMQUtil;
+
 public class ApiApp {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
+    ConnectionFactory connectionFactory = initMQ();
     // Тут пишем создание PersonApi, запуск и демонстрацию работы
+  }
+
+  private static ConnectionFactory initMQ() throws Exception {
+    return RabbitMQUtil.buildConnectionFactory();
   }
 }
--- src/main/java/io/ylab/intensive/lesson04/eventsourcing/db/DbApp.java
+++ src/main/java/io/ylab/intensive/lesson04/eventsourcing/db/DbApp.java
@@ -1,7 +1,35 @@
 package io.ylab.intensive.lesson04.eventsourcing.db;
 
+import java.sql.SQLException;
+import javax.sql.DataSource;
+
+import com.rabbitmq.client.ConnectionFactory;
+import io.ylab.intensive.lesson04.DbUtil;
+import io.ylab.intensive.lesson04.RabbitMQUtil;
+
 public class DbApp {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
+    DataSource dataSource = initDb();
+    ConnectionFactory connectionFactory = initMQ();
+
     // тут пишем создание и запуск приложения работы с БД
+  }
+  
+  private static ConnectionFactory initMQ() throws Exception {
+    return RabbitMQUtil.buildConnectionFactory();
+  }
+  
+  private static DataSource initDb() throws SQLException {
+    String ddl = "" 
+                     + "drop table if exists person;" 
+                     + "create if not exists table person (\n"
+                     + "person_id bigint primary key,\n"
+                     + "first_name varchar,\n"
+                     + "last_name varchar,\n"
+                     + "middle_name varchar\n"
+                     + ")";
+    DataSource dataSource = DbUtil.buildDataSource();
+    DbUtil.applyDdl(ddl, dataSource);
+    return dataSource;
   }
 }
